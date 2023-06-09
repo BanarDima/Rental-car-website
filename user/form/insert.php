781d7855a256a969539b4f85b82ca9f8c23ac1a6
banardima <banar.dmytro.clg@chnu.edu.ua>
2023-06-09 04:02:33 +0300

    Реєстрацію доведено до робочого стану

--- user/form/insert.php
+++ user/form/insert.php
@@ -10,8 +10,51 @@ if(isset($_POST['submit'])){
     $Password = $_POST['password'];
     
     
-mysqli_query($Con, " INSERT INTO `tbluser`(`UserName`, `Email`, `Number`, `Password`) VALUES ('$Name','$Email','$Number','$Password') ");
+    $Dup_Email = mysqli_query($Con, "SELECT * FROM `tbluser` WHERE Email = '$Email' ");
+    $Dup_UserName = mysqli_query($Con, "SELECT * FROM `tbluser` WHERE UserName= '$Name' ");
+    $Dup_Number = mysqli_query($Con, "SELECT * FROM `tbluser` WHERE Number= '$Number' ");
 
+if(mysqli_num_rows( $Dup_Email)) {
+    echo"
+    <script>
+    alert('this Email is already taken');
+    window.location.href= 'registration.php'
+    </script>
+    ";
+
+}
+
+if(mysqli_num_rows( $Dup_UserName)) {
+    echo"
+    <script>
+    alert('this Username is already taken');
+    window.location.href= 'registration.php'
+    </script>
+    ";
+
+}
+
+if(mysqli_num_rows( $Dup_Number)) {
+    echo"
+    <script>
+    alert('this Number is already taken');
+    window.location.href= 'registration.php'
+    </script>
+    ";
+
+}
+
+else {
+    mysqli_query($Con, " INSERT INTO `tbluser`(`UserName`, `Email`, `Number`, `Password`) 
+    VALUES ('$Name','$Email','$Number','$Password') ");
+
+echo"
+<script>
+alert('Register successfully');
+window.location.href= 'registration.php'
+</script>
+";
+}
 
 }
 
